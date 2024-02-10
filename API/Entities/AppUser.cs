6bad8697ffe5f45cce989ca718f314d36fe97c04
Brent Marquez <brentonmarquez@gmail.com>
2024-02-09 20:50:49 -0700

    create first entity, disable nullable in project file for optional strings

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace API;
+
+public class AppUser
+{
+    // Entity Framewok needs these properties to be public to set in db etc.
+    public int Id { get; set; }
+
+    // since C# 6 strings are assumed to be non-null, now you need to add ? to make it optional
+    // you can turn off this flag in API.csproj (project file) and changing <Nullable> flag to disable
+    public string UserName { get; set; }
+}
